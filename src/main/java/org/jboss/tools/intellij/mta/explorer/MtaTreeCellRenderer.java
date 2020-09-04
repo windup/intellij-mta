a842de6f4fc9a7dd97312982a4c1358f1f47c2fe
John Steele <programjsteele@gmail.com>
2020-09-04 15:42:35 -0400

    Add configuration and tree models to tree cell renderer to be used in actions

--- src/main/java/org/jboss/tools/intellij/mta/explorer/MtaTreeCellRenderer.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/MtaTreeCellRenderer.java
@@ -1,6 +1,24 @@
 package org.jboss.tools.intellij.mta.explorer;
 
 import com.intellij.ide.util.treeView.NodeRenderer;
+import com.intellij.ui.tree.StructureTreeModel;
+import org.jboss.tools.intellij.mta.model.MtaModel;
 
 public class MtaTreeCellRenderer extends NodeRenderer {
+
+    private MtaModel model;
+    private StructureTreeModel treeModel;
+
+    public MtaTreeCellRenderer(MtaModel model, StructureTreeModel treeModel) {
+        this.model = model;
+        this.treeModel = treeModel;
+    }
+
+    public MtaModel getModel() {
+        return this.model;
+    }
+
+    public StructureTreeModel getTreeModel() {
+        return this.treeModel;
+    }
 }
