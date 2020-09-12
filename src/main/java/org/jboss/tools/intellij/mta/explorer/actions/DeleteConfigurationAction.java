8bcc846a0faa2bb62e8095e7067a5c8a0f3da593
John Steele <programjsteele@gmail.com>
2020-09-11 20:02:23 -0400

    WINDUP-2734 - Action for deleting configuration from the mta explorer

---
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/actions/DeleteConfigurationAction.java
@@ -0,0 +1,39 @@
+package org.jboss.tools.intellij.mta.explorer.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.ui.treeStructure.Tree;
+import org.jboss.tools.intellij.mta.explorer.MtaTreeCellRenderer;
+import org.jboss.tools.intellij.mta.explorer.nodes.ConfigurationNode;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration;
+import org.jboss.tools.intellij.mta.model.MtaModel;
+
+import javax.swing.tree.TreePath;
+
+public class DeleteConfigurationAction extends StructureTreeAction {
+
+    public DeleteConfigurationAction() {
+        super(ConfigurationNode.class);
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected) {
+        ConfigurationNode node = (ConfigurationNode)super.adjust(selected);
+        MtaConfiguration configuration = (MtaConfiguration)node.getValue();
+        Tree tree = super.getTree(anActionEvent);
+        MtaTreeCellRenderer renderer = (MtaTreeCellRenderer) tree.getCellRenderer();
+        MtaModel model = renderer.getModel();
+        model.deleteConfiguration(configuration);
+        //noinspection UnstableApiUsage
+        renderer.getTreeModel().invalidate(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+
+    @Override
+    public boolean isVisible(Object[] selected) {
+        if (selected.length == 0) return false;
+        return super.isVisible(selected);
+    }
+}
