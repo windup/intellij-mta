ab26df47496d41af08d191a66c9fe2be68d752c8
John Steele <programjsteele@gmail.com>
2020-09-24 16:52:26 -0400

    add analysis results as child to configuration nodes

--- src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/ConfigurationNode.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/ConfigurationNode.java
@@ -3,9 +3,11 @@ package org.jboss.tools.intellij.mta.explorer.nodes;
 import com.google.common.collect.Lists;
 import com.intellij.ide.projectView.PresentationData;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.List;
 
 public class ConfigurationNode extends MtaExplorerNode<MtaConfiguration> {
 
@@ -16,16 +18,25 @@ public class ConfigurationNode extends MtaExplorerNode<MtaConfiguration> {
     @NotNull
     @Override
     public Collection<AnalysisResultsNode> getChildren() {
-        return Lists.newArrayList();
+        List<AnalysisResultsNode> children = Lists.newArrayList();
+        AnalysisResultsSummary summary = this.getValue().getSummary();
+        if (summary != null) {
+            children.add(new AnalysisResultsNode(summary));
+        }
+        return children;
     }
 
     @Override
     protected void update(PresentationData presentation) {
-        presentation.setPresentableText(this.getValue().getName());
+        presentation.setPresentableText(this.getText());
     }
 
     @Override
     protected boolean shouldUpdateData() {
         return true;
+    }
+
+    public String getText() {
+        return this.getValue().getName();
     }
 }
