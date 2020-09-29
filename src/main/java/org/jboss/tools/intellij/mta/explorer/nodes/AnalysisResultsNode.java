fb79a8fc762ac0731c611bbe835f0227a91ce494
John Steele <programjsteele@gmail.com>
2020-09-28 20:22:50 -0400

    setting up analysis results tree node

--- src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/AnalysisResultsNode.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/AnalysisResultsNode.java
@@ -3,15 +3,15 @@ package org.jboss.tools.intellij.mta.explorer.nodes;
 import com.google.common.collect.Lists;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
-import org.jboss.tools.intellij.mta.model.AnalysisResults;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
-public class AnalysisResultsNode extends MtaExplorerNode<AnalysisResults> {
+public class AnalysisResultsNode extends MtaExplorerNode<AnalysisResultsSummary> {
 
-    public AnalysisResultsNode(AnalysisResults analysisResults) {
-        super(analysisResults);
+    public AnalysisResultsNode(AnalysisResultsSummary summary) {
+        super(summary);
     }
 
     @NotNull
@@ -27,6 +27,6 @@ public class AnalysisResultsNode extends MtaExplorerNode<AnalysisResults> {
 
     @Override
     protected boolean shouldUpdateData() {
-        return false;
+        return true;
     }
 }
