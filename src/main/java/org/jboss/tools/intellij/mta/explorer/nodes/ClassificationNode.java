87e071dac558428aea4165ea6c7133fe7dd52a96
John Steele <programjsteele@gmail.com>
2020-09-30 09:15:40 -0400

    Explorer tree node representing a classification

---
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/ClassificationNode.java
@@ -0,0 +1,35 @@
+package org.jboss.tools.intellij.mta.explorer.nodes;
+
+import com.google.common.collect.Lists;
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.ide.util.treeView.AbstractTreeNode;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ClassificationNode extends MtaExplorerNode<Classification> {
+
+    public ClassificationNode(Classification classification) {
+        super(classification);
+    }
+
+    @NotNull
+    @Override
+    public Collection<? extends AbstractTreeNode> getChildren() {
+        List<AnalysisResultsNode> children = Lists.newArrayList();
+        return children;
+    }
+
+    @Override
+    protected void update(PresentationData presentation) {
+        presentation.setPresentableText(super.getValue().title);
+    }
+
+    @Override
+    protected boolean shouldUpdateData() {
+        return true;
+    }
+}
