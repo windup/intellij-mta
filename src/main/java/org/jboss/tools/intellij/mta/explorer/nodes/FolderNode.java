eacccf33c27d7b610eecce9ec40bad1f52c2a7f8
John Steele <programjsteele@gmail.com>
2020-10-06 19:06:16 -0400

    File and Folder nodes for explorer

---
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/FolderNode.java
@@ -0,0 +1,33 @@
+package org.jboss.tools.intellij.mta.explorer.nodes;
+
+import com.google.common.collect.Lists;
+import com.intellij.ide.projectView.PresentationData;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+
+public class FolderNode extends MtaExplorerNode<AnalysisResultsSummary> {
+
+    public FolderNode(AnalysisResultsSummary summary) {
+        super(summary);
+    }
+
+    @NotNull
+    @Override
+    public Collection<MtaExplorerNode> getChildren() {
+        List<MtaExplorerNode> children = Lists.newArrayList();
+        return children;
+    }
+
+    @Override
+    protected void update(PresentationData presentation) {
+        presentation.setPresentableText("Results");
+    }
+
+    @Override
+    protected boolean shouldUpdateData() {
+        return true;
+    }
+}
