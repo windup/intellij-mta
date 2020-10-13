8ff90e7411f503ac0dd804b1967c6970b54c760e
John Steele <programjsteele@gmail.com>
2020-10-13 19:09:57 -0400

    Add File node children to analysis results.

--- src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/AnalysisResultsNode.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/AnalysisResultsNode.java
@@ -1,13 +1,15 @@
 package org.jboss.tools.intellij.mta.explorer.nodes;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
+import org.apache.commons.io.comparator.NameFileComparator;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AnalysisResultsNode extends MtaExplorerNode<AnalysisResultsSummary> {
@@ -19,15 +21,33 @@ public class AnalysisResultsNode extends MtaExplorerNode<AnalysisResultsSummary>
     @NotNull
     @Override
     public Collection<? extends AbstractTreeNode<?>> getChildren() {
-        List<MtaExplorerNode<?>> children = Lists.newArrayList();
-        children.addAll(super.getValue().hints.stream().map(HintNode::new).collect(Collectors.toList()));
-        children.addAll(super.getValue().classifications.stream().map(ClassificationNode::new).collect(Collectors.toList()));
+        List<Issue> issues = Lists.newArrayList();
+        issues.addAll(super.getValue().hints);
+        issues.addAll(super.getValue().classifications);
+
+        Map<String, FileNode> fileMap = Maps.newHashMap();
+        issues.forEach(issue -> {
+            FileNode fileNode = fileMap.get(issue.file);
+            if (fileNode == null) {
+                fileNode = new FileNode(super.getValue(), issue.file);
+                fileMap.put(issue.file, fileNode);
+            }
+            fileNode.addIssue(issue);
+        });
+
+        List<FileNode> children = new ArrayList<>(fileMap.values());
+        children.sort((o1, o2) -> {
+            NameFileComparator comparator = new NameFileComparator();
+            return comparator.compare(o1.getFile(), o2.getFile());
+        });
+
         return children;
     }
 
     @Override
     protected void update(PresentationData presentation) {
         presentation.setPresentableText("Results");
+        presentation.setIcon(AllIcons.Actions.ShowAsTree);
     }
 
     @Override
