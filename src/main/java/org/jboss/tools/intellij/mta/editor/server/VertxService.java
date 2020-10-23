a7cba01908a9df671bab400e6700bfb6c8456122
John Steele <programjsteele@gmail.com>
2020-10-23 17:11:17 -0400

    New service for bootstrapping vertx

---
+++ src/main/java/org/jboss/tools/intellij/mta/editor/server/VertxService.java
@@ -0,0 +1,44 @@
+package org.jboss.tools.intellij.mta.editor.server;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.sockjs.SockJSBridgeOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+public class VertxService {
+
+    private Vertx vertx;
+    private EventBus eventBus;
+    private HttpServer server;
+    private Router router;
+
+    public VertxService() {
+        this.init();
+    }
+
+    private void init() {
+        this.vertx = Vertx.vertx();
+        this.eventBus = vertx.eventBus();
+        this.router = Router.router(this.vertx);
+    }
+
+    private void startServer() {
+        SockJSBridgeOptions opts = new SockJSBridgeOptions()
+                .addInboundPermitted(new PermittedOptions().setAddressRegex("to.server.*"))
+                .addOutboundPermitted(new PermittedOptions().setAddressRegex("to.client.*"));
+        Router ebHandler = SockJSHandler.create(this.vertx).bridge(opts);
+        this.router.mountSubRouter("/bus/*", ebHandler);
+        this.server = this.vertx.createHttpServer().requestHandler(this.router).listen(8077);
+    }
+
+    public Router getRouter() {
+        return this.router;
+    }
+
+    public Vertx getVertx() {
+        return this.vertx;
+    }
+}
