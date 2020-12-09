277b2e58badecfc90c8e429335669bee2a2ee38c
John Steele <josteele@redhat.com>
2020-12-09 15:16:29 -0800

    Add validation checks to mta-cli and inpput of run-analyis action.

--- src/main/java/org/jboss/tools/intellij/mta/explorer/actions/RunConfigurationAction.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/actions/RunConfigurationAction.java
@@ -4,11 +4,15 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.ui.tree.StructureTreeModel;
 import com.intellij.ui.treeStructure.Tree;
 import org.jboss.tools.intellij.mta.cli.MtaCliRunner;
+import org.jboss.tools.intellij.mta.explorer.dialog.MtaNotifier;
 import org.jboss.tools.intellij.mta.explorer.nodes.ConfigurationNode;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration;
 
 import javax.swing.tree.TreePath;
 import java.awt.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class RunConfigurationAction extends StructureTreeAction {
@@ -20,23 +24,32 @@ public class RunConfigurationAction extends StructureTreeAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected) {
         ConfigurationNode node = (ConfigurationNode)super.adjust(selected);
-        MtaConfiguration configuration = (MtaConfiguration)node.getValue();
-        // if (this.validateConfiguration(configuration)) {
-            //MtaCliRunner.run(configuration);
-        // }
+        MtaConfiguration configuration = node.getValue();
+        if (this.validateConfiguration(configuration)) {
+            MtaCliRunner.run(configuration);
+        }
     }
 
     private boolean validateConfiguration(MtaConfiguration configuration) {
-//        boolean validInput = MtaCliRunner.validateOptionExists(configuration, "input");
-//        if (!validInput) {
-//            // Notify input required
-//            return false;
-//        }
-//        boolean validOutput = MtaCliRunner.validateOptionExists(configuration, "output");
-//        if (!validOutput) {
-//            // Notify output required
-//            return false;
-//        }
-        return true;
+        boolean valid = true;
+        String cliLocation = (String)configuration.getOptions().get("mta-cli");
+        if (cliLocation == null || "".equals(cliLocation)) {
+            MtaNotifier.notifyError("Path to mta-cli executable required.");
+        }
+        else if (cliLocation != null) {
+            if (!new File(cliLocation).exists()) {
+                valid = false;
+                MtaNotifier.notifyError("Path to mta-cli executable does not exist.");
+            }
+            else if (!Files.isExecutable(Paths.get(cliLocation))) {
+                valid = false;
+                MtaNotifier.notifyError("Path to mta-cli executable is not executable.");
+            }
+        }
+        if (!configuration.getOptions().containsKey("input")) {
+            valid = false;
+            MtaNotifier.notifyError("Path to input required.");
+        }
+        return valid;
     }
 }
---
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/dialog/MtaNotifier.java
@@ -0,0 +1,16 @@
+package org.jboss.tools.intellij.mta.explorer.dialog;
+
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
+
+public class MtaNotifier {
+
+    private static final NotificationGroup NOTIFICATION_GROUP =
+            new NotificationGroup("Mta Notification", NotificationDisplayType.BALLOON, true);
+
+    public static void notifyError(String content) {
+        NOTIFICATION_GROUP.createNotification(content, NotificationType.ERROR)
+                .notify(null);
+    }
+}
