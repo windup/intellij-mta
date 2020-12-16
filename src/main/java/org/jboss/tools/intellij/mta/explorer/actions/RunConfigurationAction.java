1722c616cd0df3dff1ead6d59f0eed195866b2a8
John Steele <josteele@redhat.com>
2020-12-15 18:28:46 -0800

    Update run configuration action to latest api changes. Update visible/when condition

--- src/main/java/org/jboss/tools/intellij/mta/explorer/actions/RunConfigurationAction.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/actions/RunConfigurationAction.java
@@ -3,7 +3,9 @@ package org.jboss.tools.intellij.mta.explorer.actions;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.ui.tree.StructureTreeModel;
 import com.intellij.ui.treeStructure.Tree;
+import org.jboss.tools.intellij.mta.cli.MtaCliParamBuilder;
 import org.jboss.tools.intellij.mta.cli.MtaCliRunner;
+import org.jboss.tools.intellij.mta.cli.RunAnalysisCommandHandler;
 import org.jboss.tools.intellij.mta.explorer.dialog.MtaNotifier;
 import org.jboss.tools.intellij.mta.explorer.nodes.ConfigurationNode;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration;
@@ -13,6 +15,7 @@ import java.awt.*;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class RunConfigurationAction extends StructureTreeAction {
@@ -26,7 +29,21 @@ public class RunConfigurationAction extends StructureTreeAction {
         ConfigurationNode node = (ConfigurationNode)super.adjust(selected);
         MtaConfiguration configuration = node.getValue();
         if (this.validateConfiguration(configuration)) {
-            MtaCliRunner.run(configuration);
+            String executable = (String)configuration.getOptions().get("mta-cli");
+            try {
+                String windupHome = new File(executable).getParentFile().getParent();
+                List<String> params = MtaCliParamBuilder.buildParams(configuration, windupHome);
+                RunAnalysisCommandHandler handler = new RunAnalysisCommandHandler(
+                        anActionEvent.getProject(),
+                        configuration,
+                        executable,
+                        params);
+                handler.runAnalysis();
+            }
+            catch (Exception e) {
+                System.out.println("Error building mta-cli params");
+                e.printStackTrace();
+            }
         }
     }
 
@@ -51,5 +68,11 @@ public class RunConfigurationAction extends StructureTreeAction {
             MtaNotifier.notifyError("Path to input required.");
         }
         return valid;
+    }
+
+    @Override
+    public boolean isVisible(Object[] selected) {
+        if (selected.length == 0) return false;
+        return super.isVisible(selected);
     }
 }
