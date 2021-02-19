4de05359f32a5b8bd2fb1378c1d257c7be2be37a
John Steele <programjsteele@gmail.com>
2021-02-18 16:46:00 -0800

    Select snippet of hint code.

--- src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/IssueNode.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/IssueNode.java
@@ -1,12 +1,12 @@
 package org.jboss.tools.intellij.mta.explorer.nodes;
 
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.containers.ContainerUtil;
-import com.intellij.util.indexing.FileBasedIndex;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
 
 import java.io.File;
@@ -30,7 +30,21 @@ public abstract class IssueNode<T extends Issue> extends MtaExplorerNode<T> {
                 String vFilePath = vFile.getCanonicalPath();
                 if (vFilePath.equals(filePath)) {
                     found = true;
-                    new OpenFileDescriptor(project, vFile, 0).navigate(true);
+                    try {
+                        Issue issue = super.getValue();
+                        if (issue instanceof Hint) {
+                            Hint hint = (Hint)issue;
+                            Editor editor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, vFile, hint.lineNumber - 1, hint.column), true);
+                            int offset1 = editor.getSelectionModel().getLeadSelectionOffset();
+                            editor.getSelectionModel().setSelection(offset1, offset1+hint.length);
+                        }
+                        else {
+                            new OpenFileDescriptor(project, vFile, 0).navigate(true);
+                        }
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
