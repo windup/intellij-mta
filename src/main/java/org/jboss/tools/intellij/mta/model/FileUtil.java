5102985ee9309b776a1f2d58fedea83c6b5533dd
John Steele <programjsteele@gmail.com>
2021-02-25 19:45:21 -0800

    New utility for performing operation surrounding file IO.

---
+++ src/main/java/org/jboss/tools/intellij/mta/model/FileUtil.java
@@ -0,0 +1,47 @@
+package org.jboss.tools.intellij.mta.model;
+
+import org.eclipse.jface.text.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUtil {
+
+    public static String getLine(String file, int lineNumber) {
+        try {
+            String contents = org.apache.commons.io.FileUtils.readFileToString(new File(file));
+            IDocument document = new org.eclipse.jface.text.Document(contents);
+            IRegion region = document.getLineInformation(lineNumber);
+            return document.get(region.getOffset(), region.getLength());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static void replace(IDocument document, int lineNumber, String searchString, String replacement) {
+        try {
+            IRegion info = document.getLineInformation(lineNumber);
+            FindReplaceDocumentAdapter adapter = new FindReplaceDocumentAdapter(document);
+            IRegion search = adapter.find(info.getOffset(), searchString, true, true, false/*true*/, false);
+            if (search != null) {
+                document.replace(search.getOffset(), search.getLength(), replacement);
+            }
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static IDocument deleteLine(File file, int lineNumber) {
+        try {
+            String contents = org.apache.commons.io.FileUtils.readFileToString(file);
+            IDocument document = new Document(contents);
+            IRegion info = document.getLineInformation(lineNumber);
+            document.replace(info.getOffset(), info.getLength()+1, null);
+            return document;
+        } catch (IOException | BadLocationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
