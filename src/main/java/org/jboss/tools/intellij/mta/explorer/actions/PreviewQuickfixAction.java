89b53bea878c8aa7045d6131da2a6e9ae93dd4b5
John Steele <programjsteele@gmail.com>
2021-03-23 14:06:32 -0700

    Enhancements to previewing quickfix. Improve visibility condition logic of preview quickfix handler.

--- src/main/java/org/jboss/tools/intellij/mta/explorer/actions/PreviewQuickfixAction.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/actions/PreviewQuickfixAction.java
@@ -12,15 +12,20 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogBuilder;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.util.Pair;
+import com.intellij.ui.tree.StructureTreeModel;
 import com.intellij.ui.treeStructure.Tree;
 import org.jboss.tools.intellij.mta.explorer.MtaTreeCellRenderer;
+import org.jboss.tools.intellij.mta.explorer.dialog.MtaNotifier;
 import org.jboss.tools.intellij.mta.explorer.nodes.ConfigurationNode;
 import org.jboss.tools.intellij.mta.explorer.nodes.HintNode;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration;
 import org.jboss.tools.intellij.mta.model.MtaModel;
+import org.jboss.tools.intellij.mta.model.QuickfixUtil;
 import org.jboss.tools.intellij.mta.services.ModelService;
 
 import javax.swing.tree.TreePath;
+import java.io.File;
+import java.nio.charset.Charset;
 
 public class PreviewQuickfixAction extends StructureTreeAction {
 
@@ -34,29 +39,48 @@ public class PreviewQuickfixAction extends StructureTreeAction {
         MtaTreeCellRenderer renderer = (MtaTreeCellRenderer) tree.getCellRenderer();
         Project project = renderer.getModelService().getProject();
 
-        DocumentContent oldContent = DiffContentFactory.getInstance().create("test1");
-        DocumentContent newContent = DiffContentFactory.getInstance().create("test2");
-        SimpleDiffRequest request = new SimpleDiffRequest("Diff", oldContent, newContent, "title1", "title2");
+        HintNode node = (HintNode)super.adjust(selected);
+        MtaConfiguration.Hint hint = node.getValue();
+        try {
+            String oldValue = org.apache.commons.io.FileUtils.readFileToString(new File(hint.file));
+            MtaConfiguration.QuickFix quickfix = hint.quickfixes.size() > 1 ? hint.quickfixes.get(1) : hint.quickfixes.get(0);
+            String newValue = QuickfixUtil.getQuickFixedContent(quickfix);
 
-        request.putUserData(DiffUserDataKeys.SCROLL_TO_LINE, Pair.create(Side.RIGHT, 0));
-        request.putUserData(DiffUserDataKeys.SCROLL_TO_LINE,Pair.create(Side.LEFT, 0));
+            DocumentContent oldContent = DiffContentFactory.getInstance().create(oldValue);
+            DocumentContent newContent = DiffContentFactory.getInstance().create(newValue);
 
-        DialogBuilder diffBuilder = new DialogBuilder();
-        DiffRequestPanel diffPanel = DiffManager.getInstance().createRequestPanel(project, diffBuilder, diffBuilder.getWindow());
-        diffPanel.setRequest(request);
-        diffBuilder.setCenterPanel(diffPanel.getComponent());
-        diffBuilder.setDimensionServiceKey("FileDocumentManager.FileCacheConflict");
-        diffBuilder.addOkAction().setText("Apply Quickfix");
-        diffBuilder.addCancelAction();
-        diffBuilder.setTitle("Preview Quickfix");
-        if (diffBuilder.show() == DialogWrapper.OK_EXIT_CODE) {
-            // apply quickfix
+            SimpleDiffRequest request = new SimpleDiffRequest("Diff", oldContent, newContent, "Original", "New Changes");
+
+            request.putUserData(DiffUserDataKeys.SCROLL_TO_LINE, Pair.create(Side.RIGHT, 0));
+            request.putUserData(DiffUserDataKeys.SCROLL_TO_LINE,Pair.create(Side.LEFT, 0));
+
+            DialogBuilder diffBuilder = new DialogBuilder();
+            DiffRequestPanel diffPanel = DiffManager.getInstance().createRequestPanel(project, diffBuilder, diffBuilder.getWindow());
+            diffPanel.setRequest(request);
+            diffBuilder.setCenterPanel(diffPanel.getComponent());
+            diffBuilder.setDimensionServiceKey("FileDocumentManager.FileCacheConflict");
+            diffBuilder.addOkAction().setText("Apply Quickfix");
+            diffBuilder.addCancelAction();
+            diffBuilder.setTitle("Preview Quickfix");
+            if (diffBuilder.show() == DialogWrapper.OK_EXIT_CODE) {
+                QuickfixUtil.applyQuickfix(quickfix, project, newValue);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            MtaNotifier.notifyError("Error processing quickfix file - " + hint.file);
+            return;
         }
     }
 
     @Override
     public boolean isVisible(Object[] selected) {
         if (selected.length != 1) return false;
-        return super.isVisible(selected);
+        boolean valid = super.isVisible(selected);
+        if (!valid) return false;
+
+        HintNode node = (HintNode)super.adjust(selected[0]);
+        if (node.getValue().quickfixes.isEmpty()) return false;
+        return true;
     }
 }
