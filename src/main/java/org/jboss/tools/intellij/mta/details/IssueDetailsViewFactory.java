93127cd9df1fa22e8d1930d36aeb575e0bbc0541
John Steele <programjsteele@gmail.com>
2021-04-27 18:40:57 -0700

    Update issue details factory to create new toolwindow upon request.

--- src/main/java/org/jboss/tools/intellij/mta/details/IssueDetailsViewFactory.java
+++ src/main/java/org/jboss/tools/intellij/mta/details/IssueDetailsViewFactory.java
@@ -1,18 +1,16 @@
 package org.jboss.tools.intellij.mta.details;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.content.Content;
-import com.intellij.ui.content.ContentManager;
-import org.jboss.tools.intellij.mta.explorer.MtaToolWindow;
-import org.jboss.tools.intellij.mta.services.ModelService;
 import org.jetbrains.annotations.NotNull;
 
 public class IssueDetailsViewFactory implements ToolWindowFactory {
 
+    public static final String ID = "Issue Details";
+
     @Override
     public void createToolWindowContent(Project project, @NotNull ToolWindow toolWindow) {
+        IssueDetailsView.getInstance(project).createToolWindowContent(toolWindow);
     }
 }
