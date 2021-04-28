60c223a4efae78fa6b7ec1e0f8b31fc852f0ddbe
John Steele <programjsteele@gmail.com>
2021-04-27 18:42:01 -0700

    Add databinding to issue details view.

--- src/main/java/org/jboss/tools/intellij/mta/details/IssueDetailsView.java
+++ src/main/java/org/jboss/tools/intellij/mta/details/IssueDetailsView.java
@@ -1,14 +1,17 @@
 package org.jboss.tools.intellij.mta.details;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import javax.swing.*;
 
-import org.jboss.tools.intellij.mta.model.MtaConfiguration;
+import static org.jboss.tools.intellij.mta.model.MtaConfiguration.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -19,6 +22,7 @@ public class IssueDetailsView {
     private final Project project;
 
     private JPanel myToolWindowPanel;
+    private JLabel detailsTitle;
 
     @NotNull
     public static IssueDetailsView getInstance(@NotNull Project project) {
@@ -30,21 +34,35 @@ public class IssueDetailsView {
         this.project = project;
     }
 
+    public static void showIssueDetailsView(Issue issue, Project project) {
+        ToolWindowManager manager = ToolWindowManager.getInstance(project);
+        ToolWindow toolWindow = manager.getToolWindow(IssueDetailsViewFactory.ID);
+        if (toolWindow != null) {
+            toolWindow.activate(() -> IssueDetailsView.getInstance(project).renderIssue(issue));
+        }
+    }
+
     public void createToolWindowContent(@NotNull ToolWindow toolWindow) {
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(myToolWindowPanel, "Issue Details", false);
         toolWindow.getContentManager().addContent(content);
     }
 
-    public void updateContents(MtaConfiguration.Issue issue) {
-//        Runnable task = () -> ...;
-//        Application application = ApplicationManager.getApplication();
-//        if (application.isDispatchThread()) {
-//            task.run();
-//        }
-//        else {
-//            application.invokeLater(task);
-//        }
+    public void renderIssue(Issue issue) {
+        Runnable setModelTask = () -> {
+            this.bindIssue(issue);
+        };
+        Application application = ApplicationManager.getApplication();
+        if (application.isDispatchThread()) {
+            setModelTask.run();
+        }
+        else {
+            application.invokeLater(setModelTask);
+        }
+    }
+
+    private void bindIssue(Issue issue) {
+        this.detailsTitle.setText(issue.title);
     }
 }
 
