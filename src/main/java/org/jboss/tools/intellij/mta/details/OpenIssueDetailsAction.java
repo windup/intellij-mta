66ce998721d4e4d4164350522bded6b06a64e9c7
John Steele <programjsteele@gmail.com>
2021-04-29 16:32:41 -0700

    Update issue details view upon newly selected issues in explorer.

--- src/main/java/org/jboss/tools/intellij/mta/details/OpenIssueDetailsAction.java
+++ src/main/java/org/jboss/tools/intellij/mta/details/OpenIssueDetailsAction.java
@@ -9,7 +9,11 @@ import org.jboss.tools.intellij.mta.explorer.dialog.MtaNotifier;
 import org.jboss.tools.intellij.mta.explorer.nodes.IssueNode;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration;
 
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
+import java.util.Arrays;
 
 public class OpenIssueDetailsAction extends StructureTreeAction {
 
@@ -26,10 +30,18 @@ public class OpenIssueDetailsAction extends StructureTreeAction {
         Project project = renderer.getModelService().getProject();
         if (this.detailsView == null) {
             this.detailsView = new DetailsViewConsole(project);
+            registerListeners(tree);
         }
         IssueNode node = (IssueNode)super.adjust(selected);
-        MtaConfiguration.Issue issue = (MtaConfiguration.Issue)node.getValue();
+        this.showDetails(node);
+    }
+
+    private void showDetails(IssueNode node) {
         try {
+            if (node == null) {
+                this.detailsView.clear();
+            }
+            MtaConfiguration.Issue issue = (MtaConfiguration.Issue)node.getValue();
             this.detailsView.open(issue);
         }
         catch (Exception e) {
@@ -45,5 +57,29 @@ public class OpenIssueDetailsAction extends StructureTreeAction {
         boolean valid = super.isVisible(selected);
         if (!valid) return false;
         return true;
+    }
+
+    private void registerListeners(Tree tree) {
+        tree.getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                TreePath path = e.getNewLeadSelectionPath();
+                Object obj = path.getLastPathComponent();
+                if (obj instanceof DefaultMutableTreeNode) {
+                    Object userObj = ((DefaultMutableTreeNode) obj).getUserObject();
+                    if (userObj instanceof IssueNode) {
+                        IssueNode data = (IssueNode)userObj;
+                        showDetails(data);
+                    }
+                    else {
+                        showDetails(null);
+                    }
+                }
+            }
+        });
+    }
+
+    protected Object getSelected(Tree tree) {
+        return tree.getSelectionModel().getSelectionPath().getLastPathComponent();
     }
 }
