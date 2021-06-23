a06e9193d3f9b5f310787804cb1c69dbf1c17dbb
John Steele <programjsteele@gmail.com>
2021-06-22 17:41:36 -0700

    Start test for MTA explorer

--- src/test/java/org/jboss/tools/intellij/mta/explorer/ExplorerTest.java
+++ src/test/java/org/jboss/tools/intellij/mta/explorer/ExplorerTest.java
@@ -1,5 +1,48 @@
 package org.jboss.tools.intellij.mta.explorer;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import org.jboss.tools.intellij.mta.model.MtaConfiguration;
+import org.jboss.tools.intellij.mta.services.ModelService;
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
 public class ExplorerTest {
-    
+
+    private Project project;
+    private ModelService modelService;
+    private ToolWindow toolWindow;
+
+    @Before
+    public void before() {
+        this.project = mock(Project.class);
+        this.toolWindow = mock(ToolWindow.class);
+        modelService = new ModelService(this.project);
+        modelService.loadModel();
+    }
+
+    @Test
+    public void testInitializedTree() {
+        MtaToolWindow panel = new MtaToolWindow(modelService, project, toolWindow);
+        assertTrue(panel.getTree().isEmpty());
+    }
+
+    @Test
+    public void testPopulated() {
+        MtaToolWindow panel = new MtaToolWindow(modelService, project, toolWindow);
+        modelService.createConfiguration();
+        MtaTreeCellRenderer renderer = (MtaTreeCellRenderer)panel.getTree().getCellRenderer();
+        renderer.getTreeModel().invalidate();
+        assertFalse(panel.getTree().isEmpty());
+    }
+
+    @Test
+    public void testDefaultTargetTechnology() {
+        MtaConfiguration configuration = this.modelService.createConfiguration();
+        assertNotNull(configuration);
+        assertNotNull(configuration.getOptions().get("target"));
+    }
 }
