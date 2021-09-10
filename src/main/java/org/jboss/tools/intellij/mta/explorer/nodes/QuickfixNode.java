76b18bc2fdd1becb2b5528af71802eaabe75f5c2
John Steele <programjsteele@gmail.com>
2021-09-10 11:44:44 -0700

    Set label and icon for quickfix tree nodes.

--- src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/QuickfixNode.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/QuickfixNode.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.intellij.mta.explorer.nodes;
 
+import com.google.common.collect.Lists;
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import org.jboss.tools.intellij.mta.model.MtaConfiguration;
@@ -9,19 +11,18 @@ import java.util.Collection;
 
 public class QuickfixNode extends MtaExplorerNode<MtaConfiguration.QuickFix> {
 
-    private MtaConfiguration.QuickFix quickFix;
-
     public QuickfixNode(MtaConfiguration.QuickFix quickfix) {
         super(quickfix);
     }
 
     @Override
     public @NotNull Collection<? extends AbstractTreeNode<?>> getChildren() {
-        return null;
+        return Lists.newArrayList();
     }
 
     @Override
     protected void update(@NotNull PresentationData presentation) {
-
+        presentation.setPresentableText(super.getValue().name);
+        presentation.setIcon(AllIcons.General.BalloonInformation);
     }
 }
