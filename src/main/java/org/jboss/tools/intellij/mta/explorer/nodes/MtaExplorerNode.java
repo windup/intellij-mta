bc743a3bc2e2e768e36588975d73ca2d03fa651b
John Steele <programjsteele@gmail.com>
2021-09-14 20:16:56 -0700

    add api for node click events with more context info.

--- src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/MtaExplorerNode.java
+++ src/main/java/org/jboss/tools/intellij/mta/explorer/nodes/MtaExplorerNode.java
@@ -3,6 +3,10 @@ package org.jboss.tools.intellij.mta.explorer.nodes;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.tree.StructureTreeModel;
+import org.jboss.tools.intellij.mta.explorer.MtaTreeCellRenderer;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 
 public abstract class MtaExplorerNode<T> extends AbstractTreeNode<T> {
 
@@ -14,5 +18,9 @@ public abstract class MtaExplorerNode<T> extends AbstractTreeNode<T> {
     }
 
     public void onClick(Project project) {
+    }
+
+    public void onClick(DefaultMutableTreeNode treeNode, TreePath path, MtaTreeCellRenderer renderer) {
+
     }
 }
