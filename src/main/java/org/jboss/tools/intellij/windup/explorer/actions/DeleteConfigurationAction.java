20fd9fd95d3759c2cf685ca39170c24eda836779
midays <midays@redhat.com>
2023-06-11 13:56:26 +0300

    Added support for deleting multiple configurations once

--- src/main/java/org/jboss/tools/intellij/windup/explorer/actions/DeleteConfigurationAction.java
+++ src/main/java/org/jboss/tools/intellij/windup/explorer/actions/DeleteConfigurationAction.java
@@ -4,8 +4,10 @@
 package org.jboss.tools.intellij.windup.explorer.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.ui.treeStructure.Tree;
 import org.jboss.tools.intellij.windup.explorer.WindupTreeCellRenderer;
+import org.jboss.tools.intellij.windup.explorer.dialog.WindupNotifier;
 import org.jboss.tools.intellij.windup.explorer.nodes.ConfigurationNode;
 import org.jboss.tools.intellij.windup.model.WindupConfiguration;
 import org.jboss.tools.intellij.windup.model.WindupModel;
@@ -20,16 +22,30 @@ public class DeleteConfigurationAction extends StructureTreeAction {
     }
 
     @Override
+    public void update(AnActionEvent e) {
+        Tree tree = (Tree) e.getData(PlatformDataKeys.CONTEXT_COMPONENT);
+        boolean visible = (tree != null && tree.getSelectionPaths() != null && tree.getSelectionPaths().length >= 1);
+        e.getPresentation().setEnabledAndVisible(visible);
+    }
+    @Override
     public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected) {
-        ConfigurationNode node = (ConfigurationNode)super.adjust(selected);
-        WindupConfiguration configuration = node.getValue();
-        Tree tree = super.getTree(anActionEvent);
-        WindupTreeCellRenderer renderer = (WindupTreeCellRenderer) tree.getCellRenderer();
-        WindupModel model = renderer.getModelService().getModel();
-        model.deleteConfiguration(configuration);
-        ModelService.deleteOutput(configuration);
-        renderer.getModelService().saveModel();
-        renderer.getTreeModel().invalidate();
+        // Extract the selected nodes
+        Tree tree = getTree(anActionEvent);
+        Object[] selectedNodes = getSelectedNodes(tree);
+
+        // Iterate through each selected node
+        for (Object selectedNode : selectedNodes) {
+            ConfigurationNode node = (ConfigurationNode) super.adjust(selectedNode);
+            WindupConfiguration configuration = node.getValue();
+            WindupTreeCellRenderer renderer = (WindupTreeCellRenderer) tree.getCellRenderer();
+            WindupModel model = renderer.getModelService().getModel();
+            model.deleteConfiguration(configuration);
+            ModelService.deleteOutput(configuration);
+            renderer.getModelService().saveModel();
+            renderer.getTreeModel().invalidate();
+        }
+
+        WindupNotifier.notifyError("Configurations have been deleted");
     }
 
     @Override
